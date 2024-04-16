098f72e8cbfe46f035826ff587b0fc4a5e2548b3
Luis Martin <luis.aaron.mp17@gmail.com>
2024-04-16 16:50:40 -0500

    Model Student creado con validaciones

---
+++ models/student.mjs
@@ -0,0 +1,75 @@
+import { Model, DataTypes } from 'sequelize'
+import { sequelize } from '../utils/db.mjs'
+
+class Student extends Model {}
+
+Student.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  institutionalEmail: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      is: /^\d{10,12}@unfv\.edu\.pe$/
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      min: 12
+    }
+  },
+  personalEmail: {
+    type: DataTypes.STRING,
+    validate: {
+      isEmail: true
+    }
+  },
+  cellphone: {
+    type: DataTypes.INTEGER,
+    validate: {
+      len: [9, 9]
+    }
+  },
+  dni: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    len: [8, 8]
+  },
+  dateOfBirth: {
+    type: DataTypes.DATE
+  },
+  placeOfBirth: {
+    type: DataTypes.STRING
+  },
+  address: {
+    type: DataTypes.STRING,
+    validate: {
+      len: [20, 200]
+    }
+  },
+  dateOfAdmission: {
+    type: DataTypes.DATE
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: true,
+  modelName: 'student'
+})
+
+export default Student
