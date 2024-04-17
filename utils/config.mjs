8c3383417e515e70063ea5a83a0df6614085a08b
Luis Martin <luis.aaron.mp17@gmail.com>
2024-04-17 18:22:23 -0500

    Base de datows configurada para ser usada para ser compartida con test y development

--- utils/config.mjs
+++ utils/config.mjs
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001
 
 let DATABASE_URL
 
-if (process.env.NODE_ENV === 'development') DATABASE_URL = process.env.DATABASE_URL
+if (process.env.NODE_ENV !== 'production') DATABASE_URL = process.env.DATABASE_URL
 
 export default {
   PORT,
