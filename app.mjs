eafab3ef084689f6b62fda3683d1549b5457f80d
Luis Martin <luis.aaron.mp17@gmail.com>
2024-04-17 14:41:33 -0500

    App modularizada

---
+++ app.mjs
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express from 'express'
+import studentsRouter from './controllers/students.mjs'
+import middleware from './utils/middleware.mjs'
+
+const app = express()
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/students', studentsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+export default app
--- index.mjs
+++ index.mjs
@@ -1,19 +1,7 @@
 import 'dotenv/config'
-import express from 'express'
 import { connectToDatabase } from './utils/db.mjs'
-import studentsRouter from './controllers/students.mjs'
-import middleware from './utils/middleware.mjs'
 import logger from './utils/looger.mjs'
-
-const app = express()
-
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/students', studentsRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+import app from './app.mjs'
 
 const PORT = 3000
 const start = async () => {
