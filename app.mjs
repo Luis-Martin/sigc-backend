88508507e1c651d74b6f13ed6c2360aac4cd774f
Luis Martin <luis.aaron.mp17@gmail.com>
2024-04-24 17:51:50 -0500

    Controllador del modelo Couse agregado a la app

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import 'express-async-errors'
 import express from 'express'
 import studentsRouter from './controllers/students.mjs'
+import coursesRouter from './controllers/courses.mjs'
 import loginRouter from './controllers/login.mjs'
 import middleware from './utils/middleware.mjs'
 
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/students', studentsRouter)
+app.use('/api/courses', coursesRouter)
 app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
