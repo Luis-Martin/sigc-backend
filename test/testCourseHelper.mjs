28c7958a11d304ae3cce1f9549192cd1332b9bb0
Luis Martin <luis.aaron.mp17@gmail.com>
2024-04-29 10:03:48 -0500

    Helpers para testear el controller de Course agregado

---
+++ test/testCourseHelper.mjs
@@ -0,0 +1,94 @@
+import { Course } from '../models/index.mjs'
+
+const initialCourses = [
+  {
+    code: 100001,
+    studyCycle: 1,
+    subject: 'Introducción a la Programación',
+    typeOfCourse: 'obligatorio',
+    typeOfStudy: 'general',
+    weeksLong: 16,
+    weeklyTheoreticalHours: 4,
+    weeklyPracticalHours: 2,
+    credits: 5,
+    studyPanl: 2024
+  },
+  {
+    code: 100002,
+    studyCycle: 1,
+    subject: 'Matemáticas Discretas',
+    typeOfCourse: 'obligatorio',
+    typeOfStudy: 'general',
+    weeksLong: 16,
+    weeklyTheoreticalHours: 4,
+    weeklyPracticalHours: 2,
+    credits: 5,
+    studyPanl: 2024
+  },
+  {
+    code: 100003,
+    studyCycle: 2,
+    subject: 'Estructuras de Datos y Algoritmos',
+    typeOfCourse: 'obligatorio',
+    typeOfStudy: 'general',
+    weeksLong: 16,
+    weeklyTheoreticalHours: 4,
+    weeklyPracticalHours: 2,
+    credits: 5,
+    studyPanl: 2024
+  },
+  {
+    code: 100004,
+    studyCycle: 2,
+    subject: 'Arquitectura de Computadores',
+    typeOfCourse: 'electivo',
+    typeOfStudy: 'específico',
+    weeksLong: 16,
+    weeklyTheoreticalHours: 4,
+    weeklyPracticalHours: 2,
+    credits: 5,
+    studyPanl: 2024
+  },
+  {
+    code: 100005,
+    studyCycle: 3,
+    subject: 'Bases de Datos',
+    typeOfCourse: 'obligatorio',
+    typeOfStudy: 'específico',
+    weeksLong: 16,
+    weeklyTheoreticalHours: 4,
+    weeklyPracticalHours: 2,
+    credits: 5,
+    studyPanl: 2024
+  }
+]
+
+const nonExistingCourseId = async () => {
+  const course = await Course.create({
+    code: 987654,
+    studyCycle: 6,
+    subject: 'Redes 1',
+    typeOfCourse: 'obligatorio',
+    typeOfStudy: 'general',
+    weeksLong: 15,
+    weeklyTheoreticalHours: 4,
+    weeklyPracticalHours: 2,
+    credits: 5,
+    studyPanl: 2022
+  })
+
+  await course.destroy()
+
+  return course.id
+}
+
+const coursesInDb = async () => {
+  const courses = await Course.findAll()
+  return courses
+}
+
+export default {
+  initialCourses,
+  nonExistingCourseId,
+  coursesInDb
+}
