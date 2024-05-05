3dfc285b5d0894516cdba16469c02e8a78278501
Luis Martin <luis.aaron.mp17@gmail.com>
2024-05-05 11:36:48 -0500

    Error en la relación entre las fk de EnrolledCourse con Student y Course solucionado

--- models/index.mjs
+++ models/index.mjs
@@ -2,10 +2,10 @@ import Student from './student.mjs'
 import Course from './course.mjs'
 import EnrolledCourse from './enrolledCourse.mjs'
 
-Student.hasMany(EnrolledCourse, { foreignKey: 'id' })
+Student.hasMany(EnrolledCourse)
 EnrolledCourse.belongsTo(Student)
 
-Course.hasMany(EnrolledCourse, { foreignKey: 'id' })
+Course.hasMany(EnrolledCourse)
 EnrolledCourse.belongsTo(Course)
 
 await Student.sync({ alter: true })
